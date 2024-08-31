bdad29affd1a5a2690f9cf4eb2a88fc34d48d317
Yann Bizeul <yann@tynsoe.org>
2024-08-31 12:37:53 +0200

    change class to correct property className

--- web/src/components/CbFiles.js
+++ web/src/components/CbFiles.js
@@ -238,7 +238,7 @@ function ConfirmSVG() {
             width="16"
             height="16"
             fill="currentColor"
-            class="bi bi-check2"
+            className="bi bi-check2"
             viewBox="0 0 16 16"
         >
             <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0" />
@@ -253,7 +253,7 @@ function CancelSVG() {
             width="16"
             height="16"
             fill="currentColor"
-            class="bi bi-x"
+            className="bi bi-x"
             viewBox="0 0 16 16"
         >
             <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
